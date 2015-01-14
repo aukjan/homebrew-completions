1d4171053df9529f30f5d7a2589b172675df8c1d
Aukjan van Belkum <aukjan@vanbelkum.nl>
2015-01-14 13:31:20 +0100

    Added dependency on jq

--- nuxeoctl-completion.rb
+++ nuxeoctl-completion.rb
@@ -1,6 +1,7 @@
 require "formula"
 
 class NuxeoctlCompletion < Formula
+  depends on "jq"
   homepage "https://github.com/aukjan/nuxeoctl-bash-completion"
   url "https://raw.githubusercontent.com/aukjan/nuxeoctl-bash-completion/2b4a7874b846db8bf4f1a67c052f7c621fd37939/etc/bash_completion.d/nuxeoctl-bash-completion.bash"
   sha1 "55a193e4b6f55ae54d3099774ffbbae87139df2f"
