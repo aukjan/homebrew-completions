a348f0617ba4894e91d9746b115871f83ad1d2a6
Aukjan van Belkum <aukjan@vanbelkum.nl>
2015-01-14 12:25:02 +0100

    Fixed SHA1

--- nuxeoctl-completion.rb
+++ nuxeoctl-completion.rb
@@ -3,7 +3,7 @@ require "formula"
 class NuxeoctlCompletion < Formula
   homepage "https://github.com/aukjan/nuxeoctl-bash-completion"
   url "https://raw.githubusercontent.com/aukjan/nuxeoctl-bash-completion/2b4a7874b846db8bf4f1a67c052f7c621fd37939/etc/bash_completion.d/nuxeoctl-bash-completion.bash"
-  sha1 "2b4a7874b846db8bf4f1a67c052f7c621fd37939"
+  sha1 "55a193e4b6f55ae54d3099774ffbbae87139df2f"
   head "https://raw.githubusercontent.com/aukjan/nuxeoctl-bash-completion/master/etc/bash_completion.d/nuxeoctl-bash-completion.bash"
 
   def install
