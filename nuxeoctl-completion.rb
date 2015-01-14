7a9ac4c01eaf8ea403ca751848dac31f2146239d
Aukjan van Belkum <aukjan@vanbelkum.nl>
2015-01-14 12:23:33 +0100

    renamed

--- nuxeoctl-completion.rb
+++ nuxeoctl-completion.rb
@@ -1,5 +1,6 @@
 require "formula"
-class NuxeoCtlCompletion < Formula
+
+class NuxeoctlCompletion < Formula
   homepage "https://github.com/aukjan/nuxeoctl-bash-completion"
   url "https://raw.githubusercontent.com/aukjan/nuxeoctl-bash-completion/2b4a7874b846db8bf4f1a67c052f7c621fd37939/etc/bash_completion.d/nuxeoctl-bash-completion.bash"
   sha1 "2b4a7874b846db8bf4f1a67c052f7c621fd37939"
